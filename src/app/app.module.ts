23aff222e4ea1c6d5f23575a0939690e633fb265
Przemysław Jabłecki <prz.jab98@gmail.com>
2020-05-02 18:39:32 +0200

    Add reactive form for feedback

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,7 +15,10 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle'
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
@@ -60,6 +63,9 @@ import { PromotionService } from './services/promotion.service';
     MatInputModule,
     MatCheckboxModule,
     FormsModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+    MatSlideToggleModule,
   ],
   providers: [DishService, PromotionService, LeaderService],
   entryComponents: [LoginComponent],
