f10bac701ff3911c1f5f136926c1a24e71e96c17
Przemysław Jabłecki <prz.jab98@gmail.com>
2020-05-03 11:55:16 +0200

    Use rxjs in services

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Dish } from "../shared/dish";
 import { DISHES } from "../shared/dishes";
+import { of } from 'rxjs';
+import { delay } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -8,25 +10,16 @@ import { DISHES } from "../shared/dishes";
 export class DishService {
 
   constructor() { }
-
+  // latency simulation
   getDishes(): Promise<Dish[]> {
-    return new Promise(resolve => {
-      // latency simulation
-      setTimeout(() => resolve(DISHES), 2000);
-    });
+    return of(DISHES).pipe(delay(2000)).toPromise();
   }
 
   getDish(id: string): Promise<Dish> {
-    return new Promise(resolve => {
-      // latency simulation
-      setTimeout(() => resolve(DISHES.filter(dish => dish.id === id)[0]), 2000);
-    });
+    return of(DISHES.filter(dish => dish.id === id)[0]).pipe(delay(2000)).toPromise();
   }
 
   getFeaturedDish(): Promise<Dish> {
-    return new Promise(resolve => {
-      // latency simulation
-      setTimeout(() => resolve(DISHES.filter(dish => dish.featured)[0]), 2000);
-    });
+    return of(DISHES.filter(dish => dish.featured)[0]).pipe(delay(2000)).toPromise();
   }
 }
--- src/app/services/promotion.service.ts
+++ src/app/services/promotion.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Promotion } from '../shared/promotion';
 import { PROMOTIONS } from '../shared/promotions';
+import {of} from 'rxjs';
+import {delay} from 'rxjs/operators';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -9,23 +12,14 @@ export class PromotionService {
   constructor() { }
 
   getPromotions(): Promise<Promotion[]> {
-    return new Promise(resolve => {
-      // latency simulation
-      setTimeout(() => resolve(PROMOTIONS), 2000);
-    });
+    return of(PROMOTIONS).pipe(delay(2000)).toPromise();
   }
 
   getPromotion(id: string): Promise<Promotion> {
-    return new Promise(resolve => {
-      // latency simulation
-      setTimeout(() => resolve(PROMOTIONS.filter((promo) => (promo.id === id))[0]), 2000);
-    });
+    return of(PROMOTIONS.filter((promo) => (promo.id === id))[0]).pipe(delay(2000)).toPromise();
   }
 
   getFeaturedPromotion(): Promise<Promotion> {
-    return new Promise(resolve => {
-      // latency simulation
-      setTimeout(() => resolve(PROMOTIONS.filter(leader => leader.featured)[0]), 2000);
-    });
+    return of(PROMOTIONS.filter(leader => leader.featured)[0]).pipe(delay(2000)).toPromise();
   }
 }
