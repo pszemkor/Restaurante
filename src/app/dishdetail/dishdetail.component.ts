a076d7063125df865844d95521bcebd95b8f5fdb
Przemysław Jabłecki <prz.jab98@gmail.com>
2020-05-03 13:14:31 +0200

    Add prev/next buttons on dishdetail view

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -3,6 +3,7 @@ import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common'; //track location of page in browser
 import { Dish } from "../shared/dish";
 import { DishService } from "../services/dish.service";
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-dishdetail',
@@ -12,13 +13,25 @@ import { DishService } from "../services/dish.service";
 export class DishdetailComponent implements OnInit {
 
   dish: Dish;
+  dishIds: string[];
+  prev: string;
+  next: string;
 
   constructor(private dishService: DishService, private route: ActivatedRoute, private location: Location) { }
 
   ngOnInit(): void {
-    let id = this.route.snapshot.params['id'];
-    this.dishService.getDish(id)
-      .subscribe(dish => this.dish = dish);
+    this.dishService.getDishIds()
+      .subscribe(ids => this.dishIds = ids);
+    this.route.params
+      .pipe(switchMap(params => this.dishService.getDish(params['id'])))
+      .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); });
+  }
+
+  setPrevNext(id: string) {
+    const index = this.dishIds.indexOf(id);
+    let len = this.dishIds.length;
+    this.prev = this.dishIds[(len + index - 1) % len];
+    this.next = this.dishIds[(len + index + 1) % len];
   }
 
   goBack(): void {
